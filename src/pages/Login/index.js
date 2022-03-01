f9ece040472b8a6abb278318cc352131bbceed0b
Josias Leal <josias.leal@accenture.com>
2022-03-01 16:44:31 -0300

    Estrutura inicial da aplicação: Criação de rotas

---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Login() {
+    return (
+        <div className='login_page_container'>
+            <h1>Login Page</h1>
+        </div>
+    )
+}
---
+++ src/pages/Register/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Register() {
+    return (
+        <div>
+            <h1>Register Page</h1>
+        </div>
+    )
+}
