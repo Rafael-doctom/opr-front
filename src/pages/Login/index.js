abe770add67550f8ed25e66142c9d68fdbf64630
ielepassos <iele.passos@ccc.ufcg.edu.br>
2022-03-02 17:17:25 -0300

    login page sketch

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import LoginLogo from "../../assets/LoginLogo.png";
+import "./styles.css";
 
 export default function Login() {
-    return (
-        <div className='login_page_container'>
-            <h1>Login Page</h1>
+  return (
+    <div className="login_page_container">
+      <div className="login-form-container">
+        <div className="titles">
+          <div className="title-1">O Povo</div>
+          <div className="title-2">REQUER</div>
         </div>
-    )
+        <div className="inputs">
+          <div className="form-title">Login</div>
+          <input className="login-input" type="text" placeholder=" Email" />
+          <input className="password-input" type="password" placeholder=" Senha" />
+        </div>
+        <button className="login-button">Login</button>
+      </div>
+      <div className="login-logo">
+        <img src={LoginLogo} alt="Login image" />
+      </div>
+    </div>
+  );
 }
