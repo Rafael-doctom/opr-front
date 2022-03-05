146815f0a5bd58e233945a3d2e3f3493ac031130
Josias Leal <josias.leal@accenture.com>
2022-03-04 22:14:41 -0300

    creating modal component and adjusting menu imports

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import { CloseOutlined } from '@material-ui/icons';
 
+import './styles.css'
+
+export default function Modal({ children, closeModal }) {
+    return (
+        <div className="modal_container">
+            <div className="modal">
+                <button className="close_modal_button" onClick={closeModal}>
+                    <CloseOutlined style={{ color: "#000000" }}/> 
+                </button>
+                {children}
+            </div>
+        </div>
+    )
+}
