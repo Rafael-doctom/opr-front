f3c1196b000e0ed325f49ac8132e6c2074b676cf
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2022-03-10 01:49:40 -0300

    fix/indetations and colors

--- src/components/InitialComponents/Legislator/index.js
+++ src/components/InitialComponents/Legislator/index.js
@@ -4,7 +4,6 @@ import "./styles.css";
 import {
     
     LocationOn as LocationIcon,
-    Message as MessageIcon,
     House as ComunityIcon,
     
   } from '@material-ui/icons';
@@ -16,30 +15,6 @@ export default function Legislator() {
     return (
         <div>
             <Menu> </Menu>
-
-            <div className="location_icon_and_city">           
-            <LocationIcon fontSize = "inherit" className= "location_icon"/>
-            <div className="city">Município</div>          
-            </div>
-
-            <div className="comunity_icon_and_comunity"> 
-            <ComunityIcon fontSize = "inherit" className = "comunity_icon" />
-            <div className="comunity">Comunidade</div>
-            </div>
-            
-            <div className="city_button">
-            <select className="city_button_style">
-            <option value = "Campina Grande"> Campina Grande, PB</option>
-            <option value = "Joao Pessoa"> João Pessoa, PB</option>
-            </select>
-            </div>
-
-            <div className="comunity_button">
-            <select className="comunity_button_style">
-            <option value = "Todas">Todas</option>
-            <option value = "Centro">Centro</option>
-            </select>
-            </div>
 
             <div className="container_href">
                 <a className="initial_page_link" href="/Initial-Page">
@@ -54,11 +29,32 @@ export default function Legislator() {
                     Legislativos
                 </a>
             </div>
-        
 
-            
+            <div className="location_icon_and_city">           
+                <LocationIcon fontSize = "inherit" className= "location_icon"/>
+                <div className="city">Município</div>          
+            </div>
 
+            <div className="comunity_icon_and_comunity"> 
+                <ComunityIcon fontSize = "inherit" className = "comunity_icon" />
+                <div className="comunity">Comunidade</div>
+            </div>
             
+            <div className="city_button">
+                <select className="city_button_style">
+                    <option value = "Campina Grande"> Campina Grande, PB</option>
+                    <option value = "Joao Pessoa"> João Pessoa, PB</option>
+                </select>
+            </div>
+
+            <div className="comunity_button">
+                <select className="comunity_button_style">
+                    <option value = "Todas">Todas</option>
+                    <option value = "Centro">Centro</option>
+                </select>
+            </div>
+
+
         </div>
     )
 }
