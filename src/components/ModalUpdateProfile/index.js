14de38b6ffa0d7ed50d8a89ee80ef958111f1b3c
ielepassos <iele.passos@ccc.ufcg.edu.br>
2022-03-11 12:00:19 -0300

    add update profile's modal on menu

--- src/components/ModalUpdateProfile/index.js
+++ src/components/ModalUpdateProfile/index.js
@@ -6,6 +6,7 @@ import Modal from "../Modal";
 import "./styles.css";
 
 const ModalUpdateProfile = forwardRef((props, modalRef) => {
+  
   return (
     <Modal ref={modalRef} additionalClass="box">
       <h1>Atualização de perfil</h1>
