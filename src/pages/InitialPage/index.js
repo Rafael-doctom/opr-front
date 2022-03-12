8fe453725d1491c0cf07a6acfa52956e5cf93f07
Josias Abraão Batista Leal <josias.leal@accenture.com.br>
2022-03-12 18:56:48 -0300

    adding userHomePage initial structure concept

--- src/pages/InitialPage/index.js
+++ src/pages/InitialPage/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import UserHomePage from '../../components/InitialComponents/UserHomePage';
 
 import './styles.css';
 
 export default function InitialPage() {
     return (
         <div>
-            <h1>Initial Page</h1>
+            <UserHomePage />
         </div>
     )
 }
