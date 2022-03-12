5252a46dc2dd54e550e8a4d66336652746f6461e
Josias Abraão Batista Leal <josias.leal@accenture.com.br>
2022-03-12 19:50:35 -0300

    creating base of userHomePage

--- src/components/InitialComponents/UserHomePage/index.js
+++ src/components/InitialComponents/UserHomePage/index.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Menu from "../../Menu/index";
-// import HeaderBar from "../../HeaderBar/index";
+import HeaderBar from "../../HeaderBar/index";
 import Requirement from "../../Requirement/";
 
 import './styles.css';
+import ModalNewRequeriments from "../../ModalNewRequeriments/index";
 
 export default function UserHomePage() {
     const [requirements, setRequirements] = useState([]);
+    const newReqModalRef = useRef();
 
     useEffect(() => {
         return setRequirements(require("../../../service/requirementsJsonMock.json"))
@@ -15,17 +17,26 @@ export default function UserHomePage() {
     return (
         <div className="user_home_page_container">
             <Menu />
-            {/* <HeaderBar /> */}
+            <HeaderBar />
 
-            <h1>Seus Requerimentos</h1>
+            <section className="user_home_page_content">
+                <div className="user_home_page_title">
+                    <h1>Seus Requerimentos</h1>
+                    <button className="search_requirement_button">Buscar Requerimentos</button>
+                </div>
 
-            <section className="requirements_container">
-                {requirements.map(requirement => {
-                    return (
-                        <Requirement requirement={requirement} />
-                    )
-                })}
+                <section className="user_home_page-requirements_container">
+                    {requirements.map(requirement => {
+                        return (
+                            <Requirement requirement={requirement} />
+                        )
+                    })}
+                </section>
             </section>
+
+            <button className="new_requirement_button" onClick={() => newReqModalRef.current.openModal()}>Novo</button>
+
+            <ModalNewRequeriments ref={newReqModalRef} />
         </div>
     )
 }
