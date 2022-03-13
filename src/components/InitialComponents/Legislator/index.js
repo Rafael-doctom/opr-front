a527c524464a9526112a847fcb55db3a5c7b3470
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2022-03-13 17:52:42 -0300

    feat/ adding status cards

--- src/components/InitialComponents/Legislator/index.js
+++ src/components/InitialComponents/Legislator/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import Menu from "../../Menu";
 import HeaderBar from "../../HeaderBar/index";
+import LegislatorCard from "../../LegislatorCard/index";
 
 export default function Legislator() {
     return (
@@ -11,6 +12,10 @@ export default function Legislator() {
             <Menu> </Menu>
 
             <HeaderBar />
+                      
+            <LegislatorCard requeriments={299} analysis={274} denied={10}
+            done = {15}>  </LegislatorCard>
+                        
         </div>
     )
 }
