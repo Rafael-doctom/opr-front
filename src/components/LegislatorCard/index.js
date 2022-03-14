d816f1a4db820716b28e4ae1feac12821d0d6d3f
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2022-03-14 01:53:45 -0300

    fix/ fixing cards

--- src/components/LegislatorCard/index.js
+++ src/components/LegislatorCard/index.js
@@ -4,32 +4,26 @@ import "./styles.css";
 export default function LegislatorCard({requeriments, analysis, denied, done}) {
 
     return (
-
         <section className="legislators_cards">
             <div className="container_cards"> 
                 <h1 className="title"> Requerimentos </h1>
-                <h1 className="numberRequirements"> {requeriments}</h1>
-                <h5 className="reivindicacoes"> Reivindicações </h5>
+                <h1 className="numberRequirements"> {requeriments}</h1>               
             </div>
 
             <div className="container_cards"> 
                 <h1 className="title">  Em avaliação </h1>
-                <h1 className="numberAnalysis"> {analysis}</h1>
-                <h5 className="reivindicacoes"> Reivindicações </h5>
+                <h1 className="numberAnalysis"> {analysis}</h1>               
             </div>
 
             <div className="container_cards"> 
                 <h1 className="title"> Não aceitos </h1>
-                <h1 className="numberDenied"> {denied}</h1>
-                <h5 className="reivindicacoes" > Reivindicações </h5>
+                <h1 className="numberDenied"> {denied}</h1>             
             </div>
 
             <div className="container_cards"> 
                 <h1 className="title"> Concluídos </h1>
                 <h1 className="numberDone"> {done}</h1>
-                <h5 className="reivindicacoes"> Reivindicações </h5>
             </div>
-
         </section>
         
     )
