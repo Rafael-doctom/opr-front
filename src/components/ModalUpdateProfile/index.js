872aae0e9b59cc32e14b74e4a144afc37d76aa2d
ielepassos <iele.passos@ccc.ufcg.edu.br>
2022-03-13 22:45:25 -0300

    format file

--- src/components/ModalUpdateProfile/index.js
+++ src/components/ModalUpdateProfile/index.js
@@ -2,11 +2,11 @@ import React, { forwardRef, useState } from "react";
 import { Button, Box, Grid } from "@material-ui/core/";
 import { Avatar } from "@material-ui/core";
 import Modal from "../Modal";
-import {mockUser} from "../../service/api"
+import { mockUser } from "../../service/api";
 import "./styles.css";
 
 const ModalUpdateProfile = forwardRef((props, modalRef) => {
-  const data = mockUser 
+  const data = mockUser;
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [state, setState] = useState("");
@@ -19,20 +19,32 @@ const ModalUpdateProfile = forwardRef((props, modalRef) => {
         <Grid item xs={6} className="group">
           <Box className="input">
             <h4>Nome</h4>
-            <input value ={name} onChange={(event) => setName(event.target.value)} />
+            <input
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
           </Box>
           <Box className="input">
             <h4>Email</h4>
-            <input value ={email} onChange={(event) => setEmail(event.target.value)}/>
+            <input
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </Box>
           <Box className="group_input">
             <Box className="input">
               <h4>Estado</h4>
-              <input value ={state} onChange={(event) => setState(event.target.value)}/>
+              <input
+                value={state}
+                onChange={(event) => setState(event.target.value)}
+              />
             </Box>
             <Box className="input">
               <h4>Cidade</h4>
-              <input value ={city} onChange={(event) => setCity(event.target.value)} />
+              <input
+                value={city}
+                onChange={(event) => setCity(event.target.value)}
+              />
             </Box>
           </Box>
         </Grid>
@@ -44,7 +56,9 @@ const ModalUpdateProfile = forwardRef((props, modalRef) => {
             <h4>{data.cpf}</h4>
             <h4>{data.name}</h4>
             <h4>{data.email}</h4>
-            <h4>{data.city} - {data.state}</h4>
+            <h4>
+              {data.city} - {data.state}
+            </h4>
           </Box>
         </Grid>
         <Grid item xs={12}>
