9353f6cb9578095c5220893921c5f4f295187ab1
Josias Abraão Batista Leal <josias.leal@accenture.com.br>
2022-03-14 00:24:40 -0300

    adjustments in userHomePage

--- src/components/InitialComponents/UserHomePage/index.js
+++ src/components/InitialComponents/UserHomePage/index.js
@@ -2,17 +2,24 @@ import React, { useState, useEffect, useRef } from "react";
 import Menu from "../../Menu/index";
 import HeaderBar from "../../HeaderBar/index";
 import Requirement from "../../Requirement/";
+import ModalNewRequeriments from "../../ModalNewRequeriments/index";
+import { Search, AddCircleOutline } from "@material-ui/icons/"
 
 import './styles.css';
-import ModalNewRequeriments from "../../ModalNewRequeriments/index";
 
 export default function UserHomePage() {
     const [requirements, setRequirements] = useState([]);
+    const [requirementSearchActive, setRequirementSearchActive] = useState(false);
+    const [searchKeyword, setSearchKeyword] = useState("");
     const newReqModalRef = useRef();
 
     useEffect(() => {
         return setRequirements(require("../../../service/requirementsJsonMock.json"))
     }, [])
+
+    function searchRequirements() {
+        // TODO: Criação de funcionalidade de busca de requerimentos
+    }
 
     return (
         <div className="user_home_page_container">
@@ -22,19 +29,37 @@ export default function UserHomePage() {
             <section className="user_home_page_content">
                 <div className="user_home_page_title">
                     <h1>Seus Requerimentos</h1>
-                    <button className="search_requirement_button">Buscar Requerimentos</button>
+                    {requirementSearchActive ?
+                        <div className="search_input_container">
+                            <input 
+                                autoFocus 
+                                placeholder="Palavra chave" 
+                                onBlur={() => setTimeout(() => setRequirementSearchActive(false), 300)}
+                                defaultValue={searchKeyword}
+                                onChange={(event) => setSearchKeyword(event.target.value)}
+                            />
+                            <button onClick={() => searchRequirements()}>
+                                <Search style={{ color: "#0A68F4"}}/>
+                            </button>
+                        </div>
+                        :
+                        <button className="search_requirement_button" onClick={() => setRequirementSearchActive(true)}>Buscar Requerimentos</button>
+                    }
                 </div>
 
                 <section className="user_home_page-requirements_container">
                     {requirements.map(requirement => {
                         return (
-                            <Requirement requirement={requirement} />
+                            <Requirement key={requirement.id} requirement={requirement} />
                         )
                     })}
                 </section>
             </section>
 
-            <button className="new_requirement_button" onClick={() => newReqModalRef.current.openModal()}>Novo</button>
+            <button className="new_requirement_button" onClick={() => newReqModalRef.current.openModal()}>
+                Novo
+                <AddCircleOutline />
+            </button>
 
             <ModalNewRequeriments ref={newReqModalRef} />
         </div>
