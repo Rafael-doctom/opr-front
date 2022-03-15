684eeea134a0a6d65598d5aef72a04e7f4206deb
MatheusHG <matheus.oliveira@ccc.ufcg.edu.br>
2022-03-14 22:20:01 -0300

    chore: remove test modal

--- src/pages/FAQ/index.js
+++ src/pages/FAQ/index.js
@@ -1,17 +1,11 @@
-import React, { useRef } from 'react';
-import Modal from '../../components/ModalRequirements';
+import React from 'react';
 
 import './styles.css';
 
-import { mockRequirement } from '../../service/api';
-
 export default function FAQ() {
-    const modalRef = useRef();
     return (
         <div>
             <h1>FAQ Page</h1>
-            <Modal ref={modalRef} requirement={mockRequirement} />
-            <button onClick={() => modalRef.current.openModal()}>Clica aqui</button>
         </div>
     )
 }
