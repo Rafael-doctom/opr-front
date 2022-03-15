64c4859ca6cd8bab4ca8f55df73fa9242484f33b
Josias Abraão Batista Leal <josias.leal@accenture.com.br>
2022-03-15 00:01:21 -0300

    moving modal elements to center

--- src/components/ModalUpdateProfile/index.js
+++ src/components/ModalUpdateProfile/index.js
@@ -16,50 +16,52 @@ const ModalUpdateProfile = forwardRef((props, modalRef) => {
       <h1>Atualização de perfil</h1>
 
       <Grid container className="form">
-        <Grid item xs={6} className="group">
-          <Box className="input">
-            <h4>Nome</h4>
-            <input
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </Box>
-          <Box className="input">
-            <h4>Email</h4>
-            <input
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </Box>
-          <Box className="group_input">
+        <Grid className="form_update_and_show_infos">
+          <Grid item xs={6} className="group">
             <Box className="input">
-              <h4>Estado</h4>
+              <h4>Nome</h4>
               <input
-                value={state}
-                onChange={(event) => setState(event.target.value)}
+                value={name}
+                onChange={(event) => setName(event.target.value)}
               />
             </Box>
             <Box className="input">
-              <h4>Cidade</h4>
+              <h4>Email</h4>
               <input
-                value={city}
-                onChange={(event) => setCity(event.target.value)}
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
               />
             </Box>
-          </Box>
-        </Grid>
-        <Grid className="profile" item xs={6}>
-          <Box className="box_profile">
-            <Avatar src="/profile.png" className="profile_avatar">
-              M
-            </Avatar>
-            <h4>{data.cpf}</h4>
-            <h4>{data.name}</h4>
-            <h4>{data.email}</h4>
-            <h4>
-              {data.city} - {data.state}
-            </h4>
-          </Box>
+            <Box className="group_input">
+              <Box className="input">
+                <h4>Estado</h4>
+                <input
+                  value={state}
+                  onChange={(event) => setState(event.target.value)}
+                />
+              </Box>
+              <Box className="input">
+                <h4>Cidade</h4>
+                <input
+                  value={city}
+                  onChange={(event) => setCity(event.target.value)}
+                />
+              </Box>
+            </Box>
+          </Grid>
+          <Grid className="profile" item >
+            <Box className="box_profile">
+              <Avatar src="/profile.png" className="profile_avatar">
+                M
+              </Avatar>
+              <h4>{data.cpf}</h4>
+              <h4>{data.name}</h4>
+              <h4>{data.email}</h4>
+              <h4>
+                {data.city} - {data.state}
+              </h4>
+            </Box>
+          </Grid>
         </Grid>
         <Grid item xs={12}>
           <Button
