bd79441d3a8ad9d8590b4cb7795de5fdebee032a
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2022-03-16 11:24:39 -0300

    trying to view requirement

--- src/components/InitialComponents/Legislator/index.js
+++ src/components/InitialComponents/Legislator/index.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useRef, forwardRef } from "react";
 import "./styles.css";
 import { Link } from 'react-router-dom';
+import { Button, Box, TextField } from "@material-ui/core/";
+
+import {
+    AccountCircleRounded as AccountCircleRoundedIcon,
+    ThumbUpAltOutlined as ThumbUpAltOutlinedIcon,
+    ThumbUpAltRounded as ThumbUpAltRoundedIcon,
+    DeleteOutline as DeleteOutlineIcon,
+    CloseRounded as CloseRoundedIcon,
+    Edit as EditIcon,
+    Save as SaveIcon,
+  } from "@material-ui/icons/";
 
 import Menu from "../../Menu";
 import HeaderBar from "../../HeaderBar/index";
 import LegislatorCard from "../../LegislatorCard/index";
+import ModalComponent from "../../Modal/index";
+
 
 export default function Legislator() {
+
     return (
         <div>
             <Menu> </Menu>
@@ -15,6 +29,24 @@ export default function Legislator() {
                       
             <LegislatorCard requeriments={299} analysis={274} denied={10}
             done = {15}>  </LegislatorCard>
+
+            <section className="requirement_content">
+                
+           
+            <Box className="header-profile">
+
+            </Box>
+            
+            
+            <Box className="profile">   
+            <AccountCircleRoundedIcon color="action" />     
+                <Box className="info">
+                <h5>{"Matheus"}</h5>
+                <small>{"Campina"}</small>
+                </Box>
+            </Box>  
+
+            </section>
                         
         </div>
     )
