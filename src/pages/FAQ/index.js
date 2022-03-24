0da2cfa798485088dd1daafc848f8cd71313af5b
ielepassos <iele.passos@ccc.ufcg.edu.br>
2022-03-24 00:00:47 -0300

    add FAQ page

--- src/pages/FAQ/index.js
+++ src/pages/FAQ/index.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import Menu from "../../components/Menu";
+import HeaderBar from "../../components/HeaderBar";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-import './styles.css';
+import "./styles.css";
 
 export default function FAQ() {
-    return (
-        <div>
-            <h1>FAQ Page</h1>
-        </div>
-    )
+  return (
+    <div>
+        <Menu />
+        <HeaderBar />
+      <div className="faq_container">
+          <div className="faq_title"> Perguntas Frequentes</div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}     
+          >
+            <Typography>Qual o intuito do sistema?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+            Dar voz às comunidades para que o exercício da cidadania seja garantido, pois aquilo que o Povo Requer deve ser a força motriz do Estado.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography>O que motivou a criação do sistema?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+            A falta de um instrumento que permita que um indivíduo, de forma simples, tenha a possibilidade de levar ao conhecimento da câmara legislativa as necessidades de sua comunidade.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Typography>Insira uma pergunta</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+            resposta aqui :)
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    </div>
+  );
 }
