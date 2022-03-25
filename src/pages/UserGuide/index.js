eabdc7d0a385cfdfee8eeade138badb39f6557d3
ielepassos <iele.passos@ccc.ufcg.edu.br>
2022-03-25 19:29:43 -0300

    add page

--- src/pages/UserGuide/index.js
+++ src/pages/UserGuide/index.js
@@ -1,39 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import Menu from '../../components/Menu';
-import Carousel from 'react-material-ui-carousel';
+import Menu from "../../components/Menu";
+import Carousel from "react-material-ui-carousel";
 
-import './style.css';
+import CadastroCidadao from "../../assets/CadastroCidadao.png";
+import CadastroLegislativo from "../../assets/CadastroLegislativo.png";
+import "./style.css";
 
-export default function UserGuide(){
-    var pages = [
-        {
-            name: 'abc',
-        },
-        {
-            name: 'def',
-        },
-        {
-            name: 'ghi',
-        }
-    ]
+export default function UserGuide() {
+  var pages = [
+    {
+      name: "Se atente ao cadastro!",
+      imagea: CadastroCidadao,
+      imageb: CadastroLegislativo,
+      txt:
+        "Os cadastros para cidadão e legislador são diferentes." +
+        "Para se cadastrar corretamente, fique atento ao link de cadastro, sinalizado pelas setas laranjas nas imagens.",
+    },
+    {
+      name: "def",
+    },
+    {
+      name: "ghi",
+    },
+  ];
 
-    return(
-        <div className="page_user_guide">
-            <Menu />
-            <div className="carousel_container">
-                <Carousel autoPlay={false}>
-                    { pages.map((page, index) => <Page page={page} key={index} />)}
-                </Carousel>
-            </div>
-        </div>
-    )
+  return (
+    <div className="page_user_guide">
+      <Menu />
+      <div className="carousel_container">
+        <Carousel autoPlay={false}>
+          {pages.map((page, index) => (
+            <Page page={page} key={index} />
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
 }
 
 function Page(props) {
-    return (
-        <div>
-            <h1>{props.page.name}</h1>
+  return (
+    <div className="user_guide_container">
+      <h1>{props.page.name}</h1>
+      <div className="description">{props.page.txt} </div>
+      <div className="images">
+        <div className="image_a">
+          <img src={props.page.imagea} alt="Cadastro Cidadão"></img>
         </div>
-    )
+        <div className="image_b">
+          <img src={props.page.imageb} alt="Cadastro de Legislativo"></img>
+        </div>
+      </div>
+    </div>
+  );
 }
