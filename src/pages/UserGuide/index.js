ea0d0e98c096a9b4a06a1667edeb742bf2f8d59e
Samuel Costa <samuel.costa@ccc.ufcg.edu.br>
2022-03-25 18:34:25 -0300

    inicio do desenvolvimento

--- src/pages/UserGuide/index.js
+++ src/pages/UserGuide/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
+import Menu from '../../components/Menu';
+import Carousel from 'react-material-ui-carousel';
+
+import './style.css';
+
 export default function UserGuide(){
+    var pages = [
+        {
+            name: 'abc',
+        },
+        {
+            name: 'def',
+        },
+        {
+            name: 'ghi',
+        }
+    ]
 
     return(
+        <div className="page_user_guide">
+            <Menu />
+            <div className="carousel_container">
+                <Carousel autoPlay={false}>
+                    { pages.map((page, index) => <Page page={page} key={index} />)}
+                </Carousel>
+            </div>
+        </div>
+    )
+}
+
+function Page(props) {
+    return (
         <div>
-            userguide
+            <h1>{props.page.name}</h1>
         </div>
     )
 }
