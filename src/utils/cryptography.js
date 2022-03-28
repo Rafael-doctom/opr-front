d34a288a7efed6b81d517730e97fc6402af38597
Josias Abraão Batista Leal <josias.leal@accenture.com.br>
2022-03-27 23:53:42 -0300

    removing lib

--- src/utils/cryptography.js
+++ src/utils/cryptography.js
@@ -1,20 +1,4 @@
 import jwt_decode from "jwt-decode";
-var bcrypt = require('bcryptjs');
-
-const SALT_ROUNDS = 10;
-
-export function encryptValue(value) {
-    var salt = bcrypt.genSaltSync(SALT_ROUNDS);
-    var hash = bcrypt.hashSync(value, salt);
-
-    return hash
-}
-
-export async function compareValue(value, hash) {
-    var comparation = bcrypt.compareSync(value, hash);
-
-    return comparation;
-}
 
 export function decryptJWT(jwt) {
     var decoded = jwt_decode(jwt);
